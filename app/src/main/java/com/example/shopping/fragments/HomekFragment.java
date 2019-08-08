318d48857484aef501d763c31e98818ad65d1576
zhangyuchen886 <3444769311@qq.com>
2019-08-08 17:16:15 +0800

    首页写完了，安排

--- app/src/main/java/com/example/shopping/fragments/HomekFragment.java
+++ app/src/main/java/com/example/shopping/fragments/HomekFragment.java
@@ -10,14 +10,20 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.Toast;
 
-import com.example.shopping.R;
 import com.example.shopping.CaptureActivity;
+import com.example.shopping.MainActivity;
+import com.example.shopping.R;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -32,6 +38,11 @@ public class HomekFragment extends Fragment {
     private static final int REQUEST_CODE_SCAN = 0x0000;
     private static final String DECODED_CONTENT_KEY = "codedContent";
     private static final String DECODED_BITMAP_KEY = "codedBitmap";
+    private SearchView searchView;
+    private ListView listView;
+    //定义自动完成的列表
+    private final String[] mStrings = {"我爱Java","安卓无敌","好好学习"};
+
     public HomekFragment() {
         // Required empty public constructor
 
@@ -43,7 +54,50 @@ public class HomekFragment extends Fragment {
         // Inflate the layout for this fragment
         View inflate = inflater.inflate(R.layout.fragment_homek, null);
         initView(inflate);
+        initSearchView();
         return inflate;
+    }
+
+    private void initSearchView() {
+        final ArrayAdapter adapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,mStrings);
+        listView.setAdapter(adapter);
+        //为ListView启动过滤
+        listView.setTextFilterEnabled(true);
+
+        //设置SearchView自动缩小为图标
+        searchView.setIconifiedByDefault(false);//设为true则搜索栏 缩小成俄日一个图标点击展开
+        //设置该SearchView显示搜索按钮
+        searchView.setSubmitButtonEnabled(true);
+        //设置默认提示文字
+        searchView.setQueryHint("搜索");
+        //配置监听器
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            //点击搜索按钮时触发
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //此处添加查询开始后的具体时间和方法
+                Toast.makeText(getActivity(),"搜索内容为:" + query,Toast.LENGTH_SHORT).show();
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //如果newText长度不为0
+                if (TextUtils.isEmpty(newText)){
+                    listView.clearTextFilter();
+                }else{
+                    listView.setFilterText(newText);
+//          adapter.getFilter().filter(newText.toString());//替换成本句后消失黑框！！！
+                }
+                return true;
+            }
+        });
+        listView.setOnItemClickListener(new ListView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Object string = adapter.getItem(position);
+                searchView.setQuery(string.toString(),true);
+            }
+        });
     }
 
     private void initView(View inflate) {
@@ -59,6 +113,25 @@ public class HomekFragment extends Fragment {
                 }
             }
         });
+        searchView = (SearchView) inflate.findViewById(R.id.searchView);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            // 当点击搜索按钮时触发该方法
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            // 当搜索内容改变时触发该方法
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Toast.makeText(getActivity(), "aaaaa", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+
+        listView = (ListView) inflate.findViewById(R.id.lv);
     }
 
     private void goScan() {
@@ -92,7 +165,7 @@ public class HomekFragment extends Fragment {
                 //返回的BitMap图像
                 Bitmap bitmap = data.getParcelableExtra(DECODED_BITMAP_KEY);
 
-                Toast.makeText(getActivity(), "你扫描到的内容是："+content, Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), "你扫描到的内容是：" + content, Toast.LENGTH_SHORT).show();
             }
         }
     }
