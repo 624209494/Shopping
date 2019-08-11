2de75a77dc7a63f22c3265acbda8c33379b8fa19
1543749470 <1543749470@qq.com>
2019-08-11 14:34:49 +0800

    分类结构

---
+++ app/src/main/java/com/example/shopping/adapters/MyPagerAdapter.java
@@ -0,0 +1,27 @@
+package com.example.shopping.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class MyPagerAdapter extends FragmentPagerAdapter {
+    ArrayList<Fragment> list;
+
+
+    public MyPagerAdapter(FragmentManager fm, ArrayList<Fragment> list) {
+        super(fm);
+        this.list = list;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return list.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+}
